6874765c9076ba9ad56b8e8bbf979a4c728ee743
Yanni Kashoqa <YANNI_KASHOQA@msn.com>
2019-03-06 16:58:33 -0500

    Initial Commit

---
+++ Get-SOAP_Object_By_HostID.ps1
@@ -0,0 +1,58 @@
+Clear-Host
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+$Credentials = (Get-Content "$PSScriptRoot\DS-Config.json" -Raw) | ConvertFrom-Json
+
+$Manager = $Credentials.MANAGER
+$Port = $Credentials.PORT
+$Tenant = $Credentials.TENANT
+$UserName = $Credentials.USER_NAME
+$Password = $Credentials.PASSWORD
+$APIKEY = $Credentials.APIKEY
+
+$ErrorActionPreference = 'Stop'
+
+$WSDL = "/webservice/Manager?WSDL"
+$DSM_URI = "https://" + $Manager+ ":" + $Port + $WSDL
+
+#$HostName = "lt-ubuntu18"
+$HostID = "4601"
+$sID = "5BF3CE88-CDD2-69DD-ADA0-AF26F3B00760_681A287D7AA1866F769F780B23B68AFF"
+
+[xml] $SoapRequest = '
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:Manager">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <urn:hostDetailRetrieve>
+         <urn:hostFilter>
+            <urn:hostID></urn:hostID>
+            <urn:type>SPECIFIC_HOST</urn:type>
+         </urn:hostFilter>
+         <urn:hostDetailLevel>LOW</urn:hostDetailLevel>
+         <urn:sID></urn:sID>
+      </urn:hostDetailRetrieve>
+   </soapenv:Body>
+</soapenv:Envelope>
+'
+
+$SoapRequest.Envelope.Body.hostDetailRetrieve.hostFilter.hostID = $HostID
+$SoapRequest.Envelope.Body.hostDetailRetrieve.sID = $sID
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "text/xml")
+$headers.Add("soapaction", "hostDetailRetrieve")
+
+[xml] $obj_Manager = Invoke-WebRequest -Uri $DSM_URI -Headers $headers -Method Post -Body $SoapRequest -SkipHeaderValidation
+
+$Results = $obj_Manager.Envelope.Body.hostDetailRetrieveResponse.hostDetailRetrieveReturn
+$LastRecommendationScan = $Results.overallLastRecommendationScan
+
+If ($LastRecommendationScan.IsEmpty){
+   Write-Host "No Recommendation scan exist"
+}Else{
+   Write-Host $LastRecommendationScan
+}
+
+
+
---
+++ Get-SOAP_Object_By_Name.ps1
@@ -0,0 +1,53 @@
+Clear-Host
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+$Credentials = (Get-Content "$PSScriptRoot\DS-Config.json" -Raw) | ConvertFrom-Json
+
+$Manager = $Credentials.MANAGER
+$Port = $Credentials.PORT
+$Tenant = $Credentials.TENANT
+$UserName = $Credentials.USER_NAME
+$Password = $Credentials.PASSWORD
+$APIKEY = $Credentials.APIKEY
+
+$ErrorActionPreference = 'Stop'
+
+$WSDL = "/webservice/Manager?WSDL"
+$DSM_URI = "https://" + $Manager+ ":" + $Port + $WSDL
+
+$HostName = "10.0.0.178"
+$sID = "5BF3CE88-CDD2-69DD-ADA0-AF26F3B00760_681A287D7AA1866F769F780B23B68AFF"
+
+[xml] $SoapRequest = '
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:Manager">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <urn:hostDetailRetrieveByName>
+         <urn:hostname></urn:hostname>
+         <urn:hostDetailLevel>LOW</urn:hostDetailLevel>
+         <urn:sID></urn:sID>
+      </urn:hostDetailRetrieveByName>
+   </soapenv:Body>
+</soapenv:Envelope>
+'
+
+$SoapRequest.Envelope.Body.hostDetailRetrieveByName.hostname = $HostName
+$SoapRequest.Envelope.Body.hostDetailRetrieveByName.sID = $sID
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "text/xml")
+$headers.Add("soapaction", "hostDetailRetrieveByName")
+
+[xml] $obj_Manager = Invoke-WebRequest -Uri $DSM_URI -Headers $headers -Method Post -Body $SoapRequest -SkipHeaderValidation
+
+$Results = $obj_Manager.Envelope.Body.hostDetailRetrieveByNameResponse.hostDetailRetrieveByNameReturn
+
+$LastRecommendationScan = $Results.overallLastRecommendationScan
+
+If ($LastRecommendationScan.IsEmpty){
+   Write-Host "No Recommendation scan exist"
+}Else{
+   Write-Host $LastRecommendationScan
+}
+
